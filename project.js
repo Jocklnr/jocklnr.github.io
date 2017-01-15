6ec1bccaf5908e0a4aca7ed3a4d601cf4b5d9d2e
Jock Lanier <jocklnr@gmail.com>
2017-01-15 16:54:45 -0500

    initial upload

---
+++ project.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+$(".toggleButtons").hover(function() {
+
+	$(this).addClass("hover");
+
+}, function() {
+
+	$(this).removeClass("hover");
+
+});
+
+$(".toggleButtons").click(function() {
+
+	$(this).toggleClass("active");
+	$(this).removeClass("hover");
+	
+	var panelId = "#" + $(this).attr("id") + "Panel";
+	
+	$(panelId).toggleClass("hidden");
+
+});
+
+$("textarea").on("change keyup paste", function() {
+	
+	updateOutput();
+
+});
+
+function updateOutput() {
+
+	$("#outputPanel").contents().find("html").html("<html><head><style type='text/css'>" + $("#cssPanel").val() + "</style></head><body>" + $("#htmlPanel").val() + "</body></html>");
+	document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val());
+
+}
+
+$(function() {
+
+	$(".panel").height($(document).height() - $("#titleBar").height() - 1);
+	updateOutput();
+
+});
+});
